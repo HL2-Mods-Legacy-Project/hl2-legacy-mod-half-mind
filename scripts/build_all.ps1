817bd3e1ac10047f6a3a81ee90de7c4e7feab16e
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:01:23 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Half-Mind" `
+  -ModName "Half-Mind" `
+  -ModFolder "Half-Mind" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/half-mind/Half-Mind-Original.zip" `
+  -ModBaseFilesUrlHash "562c2d3d5bcc6304ca466ee828bd5334e2d5768281a3257ea23b53b31c7aac3b" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
